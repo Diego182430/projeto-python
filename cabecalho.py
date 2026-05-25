77d0868c0f4f10c28386b3c97b0b69535bcdf87f
Diego182430 <00001135502225sp@al.educacao.sp.gov.br>
2026-05-25 12:18:11 +0000

    1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### Definido e Chamado a Função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print ('Versão 1.0 = desenvolvido no codespace')
+    print('=' * 30)
+
+### chamando a função 
+exibir_cabecalho()
+print('Menu principal:')
